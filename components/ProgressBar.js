892523961a7f6829ac5b51cae68d4bff534ff4bb
Debsankar Pal <debsankarpal99@gmail.com>
2025-04-19 17:52:25 +0530

    Update ProgressBar.js

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ProgressBar = ({ progress }) => {
   return (
     <div className="w-full bg-gray-200 rounded-full h-2.5 mt-2">
-      <div 
+      <div
         className="bg-blue-600 h-2.5 rounded-full transition-all duration-300 ease-in-out"
         style={{ width: `${progress}%` }}
       ></div>
